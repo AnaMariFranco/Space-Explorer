bb5b4e8b44d7039fcf36120df3bef788e5d32b19
AnaMariFranco <161780634+AnaMariFranco@users.noreply.github.com>
2024-04-17 04:36:05 -0300

    Add files via upload
    Mudançade jogo, adicionar som e quase finalização do site

---
+++ ghost maze/ghost maze/The ghost maze/script.js
@@ -0,0 +1,7 @@
+function mostrarMenu () { 
+    //Guardar o elemento HTML para adicionar a classe//
+    var menu = document.querySelector(".header-nav");
+    //Pega uma classe - toggle troca .show//
+    menu.classList.toggle("show");
+
+}
